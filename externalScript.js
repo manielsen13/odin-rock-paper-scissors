55810b7ff2c9a2d48ce088781900c00a03b57f55
manielsen13 <marshall.niels@gmail.com>
2022-08-24 11:22:55 -0400

    Finish Core Functionality of Rock Paper Scissors
    Get player choice. Change logic for checking for valid selection. Add game score and outcome announcer. Create simple scoreboard system.

--- externalScript.js
+++ externalScript.js
@@ -1,5 +1,7 @@
 console.log("Hello World");
 
+game();
+
 function getComputerChoice() {
     //generate random number between 0 and 100
     let randomNumber = (Math.random() * 100);
@@ -17,9 +19,9 @@ function getComputerChoice() {
 }
 
 function playRound (playerSelection, computerSelection) {
-
-    //chewck for invalid player inputs
+    //check for invalid player inputs
     if (!validSelection(playerSelection)) {
+        console.log("YOU HAD A SPELLING ERROR IN YOUR CHOICE AND WASTED A ROUND. TRY AGAIN.")
         return "invalidInput"
     }
 
@@ -41,19 +43,34 @@ function playRound (playerSelection, computerSelection) {
 }
 
 function game() {
-    //score tallies
+    //start of game
+    console.log("Welcome to a game of Rock, Paper, Scissors. There will be 5 rounds!")
+
+    //scoreboard
     let playerScore = 0;
     let computerScore = 0;
 
-
-
-
+    //play 5 rounds
     for (let i = 0; i < 5; i++) {
+        //get player's choice
+        let playerChoice = prompt("What's your choice: Rock, Paper, or Scissors?").toLowerCase()
         //get the outcome of the game, which could be player win, invalid, computer win, or draw
-        let outcome = playRound();
-        //awards popint to winner of round
-        //resets game if 3/5 has been won
+        let outcome = playRound(playerChoice, getComputerChoice());
+        //give a point to the winner
+        if (outcome == "computerWin") {
+            computerScore++;
+        }
+        if (outcome == "playerWin") {
+            playerScore++;
+        }
+        //announce the game score
+        console.log("THE SCORE IS");
+        console.log("PLAYER: " + playerScore);
+        console.log ("COMPUTER: " + computerScore);
     }
+
+    //announce final outcome
+    announceGameOutcome(playerScore, computerScore);
 }
 
 //use prompt() to get values for the person
@@ -61,12 +78,33 @@ function game() {
 
 
 
-//checks to see if the player entered the correct value
-function validSelection (selection) {
-    if (selection != "rock" || selection != "paper" || selection != "scissors") {
-        return false;
+
+
+
+
+function announceGameOutcome (playerFinal, computerFinal) {
+    console.log("THE GAME IS OVER! THE FINAL SCORE IS");
+    console.log("PLAYER: " + playerFinal);
+    console.log ("COMPUTER: " + computerFinal);
+
+    if (playerFinal > computerFinal) {
+        console.log("YOU WIN! CONGRATULATIONS!")
+    }
+    else if (computerFinal > playerFinal) {
+        console.log("YOU LOSE. BETTER LUCK NEXT TIME.")
     }
     else {
+        console.log("IT'S A TIE.")
+    }
+}
+
+
+//checks to see if the player entered the correct value
+function validSelection (selection) {
+    if (selection == "rock" || selection == "paper" || selection == "scissors") {
         return true;
+    }
+    else {
+        return false;
     }
 }
