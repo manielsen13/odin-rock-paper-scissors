fb67f932ef98c6a9391e34682d24f2af4e72ecf6
manielsen13 <marshall.niels@gmail.com>
2022-08-26 14:23:57 -0400

    Implement Full Game Loop
    Stop game when either player or computer reach score of 5. Create function that checks for winner. Generate reset button that refreshes page when pressed after game ends.

--- externalScript.js
+++ externalScript.js
@@ -1,6 +1,3 @@
-console.log("Hello World");
-
-
 //core components for scoreboard functionality
 let playerScore = 0;
 let computerScore = 0;
@@ -8,24 +5,32 @@ let computerScore = 0;
 const computerScoreValueDisplay = document.querySelector(".computerScoreValue");
 const playerScoreValueDisplay = document.querySelector(".playerScoreValue");
 
-
-
 //core components for announcer functionality
-const gameStartText = "Welcome to Rock, Paper, Scissors! You're playing against the computer. First to 5 wins!";
-const gameEndText = "That's the game! Press any of the three buttons to reset and play again";
+const gameStartText = "Welcome to Rock, Paper, Scissors! You're playing against the computer. First to a score of 5 wins!";
+const gameEndText = "That's the game! Press the reset button to play again!";
 
 const announcerText = document.querySelector(".announcerText");
 
-//button functionality
+//player choice buttons functionality
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 
-//round is played when buttons are pressed
+//round is played when buttons are pressed, unless a player has reached 5--then it resets
 for (const choiceBtn of choiceBtns) {
     choiceBtn.addEventListener("click", (e) => {
-        console.log(e.target.id);
-        console.log(playRound(e.target.id, getComputerChoice()))
+        if (playerScore == 5 || computerScore == 5) {
+            return;
+        }
+        playRound(e.target.id, getComputerChoice())
     });
 }
+
+//reset button + functionality
+const resetButton = document.createElement("button");
+
+resetButton.textContent = "Reset"
+resetButton.addEventListener("click", (e) => {
+    window.location.reload();
+})
 
 function getComputerChoice() {
     let randomNumber = (Math.random() * 100);
@@ -50,84 +55,21 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "rock") {
         announcerText.textContent = "You win that round!";
         playerScore++;
-        console.log(playerScore)
         playerScoreValueDisplay.textContent = playerScore.toString();
+        checkForWin();
     }
     else {
         announcerText.textContent = ("You lose that round.");
         computerScore++;
-        console.log(computerScore)
         computerScoreValueDisplay.textContent = computerScore.toString();
+        checkForWin();
     }
 }
 
-function game() {
-    //start of game
-    console.log("Welcome to a game of Rock, Paper, Scissors. There will be 5 rounds!")
-
-    //scoreboard
-    let playerScore = 0;
-    let computerScore = 0;
-
-    //play 5 rounds
-    for (let i = 0; i < 5; i++) {
-        let outcome = playRound(getPlayerChoice(), getComputerChoice());
-
-        if (outcome == "computerWin") {
-            computerScore++;
-        }
-        if (outcome == "playerWin") {
-            playerScore++;
-        }
-        //announce the game score
-        console.log("THE SCORE IS");
-        console.log("PLAYER: " + playerScore);
-        console.log ("COMPUTER: " + computerScore);
-    }
-
-    announceGameOutcome(playerScore, computerScore);
-}
-
-function getPlayerChoice() {
-    let i = 0;
-    //tries infinitely until the player enters a valid value
-    while (i < 1) {
-        let choice = prompt("What's your choice: Rock, Paper, or Scissors?");
-        if (choice == null) {
-            continue;
-        }
-        if (validSelection(choice.toString().toLowerCase())) {
-            return choice.toString().toLowerCase()
-        }
-        else {
-            console.log("Try picking again")
-        }
+function checkForWin () {
+    if (playerScore > 4 || computerScore > 4) {
+        announcerText.textContent = gameEndText;
+        document.body.appendChild(resetButton);
     }
 }
 
-//runs when the game is over
-function announceGameOutcome (playerFinal, computerFinal) {
-    console.log("THE GAME IS OVER! THE FINAL SCORE IS");
-    console.log("PLAYER: " + playerFinal);
-    console.log ("COMPUTER: " + computerFinal);
-
-    if (playerFinal > computerFinal) {
-        console.log("YOU WIN! CONGRATULATIONS!")
-    }
-    else if (computerFinal > playerFinal) {
-        console.log("YOU LOSE. BETTER LUCK NEXT TIME.")
-    }
-    else {
-        console.log("IT'S A TIE.")
-    }
-}
-
-//checks to see if the player entered the correct value
-function validSelection (selection) {
-    if (selection == "rock" || selection == "paper" || selection == "scissors") {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
